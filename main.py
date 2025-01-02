f0ba2691ca55aa5a1f700b23559ca831b83fed7d
Ercanckkr <ercan.cakir@std.yeditepe.edu.tr>
2025-01-02 16:48:01 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-alter=int(input("Alter:"))
-note=float(input("Abschlussnote:"))
-if alter>=20 and alter<=50 and note>=80 and note<=100:
-  print("einstellen.")
-else:
-  print("ablehnen.")
+x1=int(input("x1:"))
+x2=int(input("x2:"))
+y1=int(input("y1:"))
+y2=int(input("y2:"))
+xdeger=x2-x1
+ydeger=y2-y1
+hip=(xdeger**2+ydeger**2)**(0.5)
+print(hip)
