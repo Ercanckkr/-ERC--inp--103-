e4f4b19269d3747ce4774e83ed3c58758220a62c
Ercanckkr <ercan.cakir@std.yeditepe.edu.tr>
2025-01-02 17:45:16 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# Kullanıcıdan bir sayı al
-sayi = input("Bir sayı girin: ")
-
-# Kullanıcının geçerli bir sayı girdiğinden emin olun
-if sayi.isdigit():
-    sayi = int(sayi)
-    
-    # Sayıyı büyükten küçüğe kadar sıralayıp toplama işlemi yap
-    toplam = 0
-    for i in range(sayi, -1, -1):  # Büyükten küçüğe doğru sıralama
-        toplam += i
-
-    # Sonucu ekrana yazdır
-    print("{} sayısına kadar büyükten küçüğe sıralanıp toplamları: {}".format(sayi, toplam))
-else:
-    print("Lütfen geçerli bir tam sayı girin.")
+import turtle 
+ninja=turtle.Turtle()
+ninja.shape("turtle")
+ninja.color("red")
+seite=int(input("seitenlaenge="))
+for i in range(4) : 
+  ninja.forward(seite)
+  ninja.right(90)
+for i in range(3):
+  ninja.forward(seite)
+  ninja.left(120)
